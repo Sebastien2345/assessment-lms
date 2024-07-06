d47040492864480207037871104cb8162e762e2f
Pauline Ann Bautista <paulineannbautista@gmail.com>
2024-07-06 14:18:26 +0800

    added landing page

---
+++ Landing Page (Professor)/includes/get_assessment_details_prof.php
@@ -0,0 +1,16 @@
+<?php
+    require_once 'config_session_inc.php';
+    require_once "dbh_inc.php";
+    require_once 'execute_query_inc.php';
+
+    // fetch assessments
+    $query_assessment = "SELECT assessment_id, subject_Code, assessment_name, date_opened, date_closed FROM assessment";
+    $result = executeQuery($mysqli, $query_assessment);
+
+    $assessments = array();
+
+    while($row = $result['result'] ->fetch_assoc()) {
+        $assessments[] = $row;
+    }
+    echo json_encode($assessments);
+?>
